167ff45638f95480dbf1ee10ed75c853e85f6d6c
Julien Perradin <julien.perradin@gmail.com>
2024-04-07 22:03:24 +0200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexus-cat',
-    version='0.1.4',
+    version='0.1.5',
     description='Nexus is a Cluster Analysing Toolkit package for atomic systems.',
     author='Julien Perradin',
     author_email='julien.perradin@umontpellier.fr',
